42016b7376da4d4023918d8465faca5d153df25f
raniele21 <ranyellesantos686@gmail.com>
2025-02-04 14:59:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return<h1>Zeus, mia e romeu, amores da minha vida. Amo todos infinitamente!</h1>
+   return<h1>Zeus, mia e romeu, amores da minha vida. Amo todos!</h1>
 }
 
 export default Home;
