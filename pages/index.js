cdf3183d0bea73572ed476f165d70adc7a99c297
raniele21 <ranyellesantos686@gmail.com>
2025-02-04 20:59:30 +0000

    modifica frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,55 @@
 function Home() {
-   return<h1>Gessica Daiane Lima, voce foi intimada. Favor tomar providencia... </h1>
+   return<h1>eu ja tive algun amores... 
+      uns terminaram com briga,
+      outros terminaram de boa,
+      e ate somos amigos ainda
+
+      alguns eu pensei que eram para vida
+      outros claramente era so uma fase
+      alguns me apeguei de verdade
+      oustros eram comodidade, mas
+      nenhum fim doeu igual
+      ao final de quase
+
+      por esse quase 
+      eu quase morri de saudade
+      eu quase bebi a cidade inteira
+      eu quase perdi a cabeça
+
+      inventando possiveis defeitos
+      que eu nem cheguei a conhecer
+      pra ver se doi so um pouco menos
+      perder quem eu nem cheguei a ter
+
+
+      esquecer o ex, ja e puxado
+      tentar esquecer 
+      um quase algo
+      tentar esquecer um quase algo
+
+      mas nenhum fim doeu igual
+      ao final desse quase
+
+      por esse quase 
+      eu quase morri de saudade
+      eu quase bebi a cidade ineteira
+      eu quase perdi a cabeça
+
+      inventando possiveis defeitos
+      que eu nem chueguei a conhecer
+      pra ver se doi so um pouco menos
+      perder quem eu nem cheguei a ter
+
+      esquecer o ex, ja e puxado
+      tentar esquecer 
+      o quase algo
+
+      ja e puxado 
+      tentar esquecer 
+      o quase algo
+
+      QUASE ALGO: HENRIQUE E JULIANO
+       </h1>
 }
 
 export default Home;
