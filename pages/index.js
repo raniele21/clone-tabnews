fdfbd0a816ddd2b317bacbb8f4e749a5579c54b5
raniele21 <ranyellesantos686@gmail.com>
2025-02-14 21:20:27 +0000

    adiciona arquivo pages

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Love never dies. It only transforms...</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
